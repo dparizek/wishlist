0b22ad5aaf5e0b5b33f7a6f96384a47dcc508f94
maryhipp <maryhipp@gmail.com>
2014-08-02 16:00:00 -0400

    reworked controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,15 +25,10 @@ class ItemsController < ApplicationController
   # POST /items.json
   def create
     @item = Item.new(item_params)
-
-    respond_to do |format|
-      if @item.save
-        format.html { redirect_to @item, notice: 'Item was successfully created.' }
-        format.json { render :show, status: :created, location: @item }
-      else
-        format.html { render :new }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.save
+      render json: model
+    else
+      render json: @item.errors, status: :unprocessable_entity
     end
   end
 
